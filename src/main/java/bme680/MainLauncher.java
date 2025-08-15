7e5c773f841aded1b966615799866171d58f5080
mati <matwoj@tutanota.com>
2025-08-15 15:05:20 +0200

    fix

--- src/main/java/bme680/MainLauncher.java
+++ src/main/java/bme680/MainLauncher.java
@@ -2,6 +2,7 @@ package bme680;
 
 public class MainLauncher {
 	public static void main(String[] args) {
+		System.out.println("Start");
 		BME680Reader reader = new BME680Reader(30);
 		SensorDataHandler sdh = new SensorDataHandler();
 		reader.addListener(sdh);
