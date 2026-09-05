b1344c54ef03071f7eadf3995514a168379db24f
mati <matwoj@tutanota.com>
2026-09-05 02:42:04 +0200

    refactor: Implement AutoCloseable to BME680Reader

--- src/main/java/com/github/nasbru/sensors/BME680Reader.java
+++ src/main/java/com/github/nasbru/sensors/BME680Reader.java
@@ -20,7 +20,7 @@ import java.util.concurrent.TimeUnit;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
-public class BME680Reader implements Runnable {
+public class BME680Reader implements Runnable, AutoCloseable {
 	private static final String LINE0 = "2025-02-28 10:58:23,[IAQ (0)]: 25.00,[T degC]: 0.00,[H %rH]: 0.00,[P hPa]: 0.00,[G Ohms]: 22477,[S]: 0,[eCO2 ppm]: 0.00,[bVOCe ppm]: 0.00";
 	private static final Logger LOGGER = LoggerFactory.getLogger(BME680Reader.class);
 
@@ -133,6 +133,11 @@ public class BME680Reader implements Runnable {
 		for (SensorListener listener : listeners) {
 			listener.onDataReceived(measurement);
 		}
+	}
+
+	@Override
+	public void close() {
+		stopReading();
 	}
 
 	public void stopReading() {
